2f6f2e5b88702606029f6ce4577262227ea2ff54
Madhavi Chauhan <madhavi@tappp.com>
2023-11-27 15:24:18 +0530

    added swift library

---
+++ TapppPanelLibrary/Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+//import PackageDescription
+//
+//let package = Package(
+//    name: "TapppPanelLibrary",
+//    products: [
+//        // Products define the executables and libraries a package produces, making them visible to other packages.
+//        .library(
+//            name: "TapppPanelLibrary",
+//            targets: ["TapppPanelLibrary"]),
+//    ],
+//    targets: [
+//        // Targets are the basic building blocks of a package, defining a module or a test suite.
+//        // Targets can depend on other targets in this package and products from dependencies.
+//        .target(
+//            name: "TapppPanelLibrary"),
+//        .testTarget(
+//            name: "TapppPanelLibraryTests",
+//            dependencies: ["TapppPanelLibrary"]),
+//    ]
+//)
+
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TapppPanelLibrary",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TapppPanelLibrary",
+            targets: ["TapppPanelLibrary"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TapppPanelLibrary",
+            resources: [
+                .copy("dist")
+            ]
+        ),
+        .testTarget(
+            name: "TapppPanelLibraryTests",
+            dependencies: ["TapppPanelLibrary"]),
+    ]
+)
+
